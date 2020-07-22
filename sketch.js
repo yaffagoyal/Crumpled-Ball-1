cdb1969bc79f0d637a1eb6a982fc124da183fbc0
yaffagoyal <67832298+yaffagoyal@users.noreply.github.com>
2020-07-22 20:43:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,7 +46,7 @@ function draw() {
 
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
-		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:95,y:-95});
 	 }
    }
 
